76949444e201e38dedcb4209da633c6072a477ec
Maïwen <ainarimanana197@gmail.com>
2026-08-20 15:44:47 +0300

    first commit

---
+++ src/test/java/org/example/examenpoo/ExamenPooApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.examenpoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenPooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
